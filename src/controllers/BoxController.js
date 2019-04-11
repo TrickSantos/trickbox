a7117bbe32c033a337fbd93d7214e202adf098fb
Patrick Adan <patrick.tafa@gmail.com>
2019-04-10 22:18:24 -0300

    Estrutura Back-end

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,18 @@
+const Box = require('../models/Box');
+
+class BoxController{
+    async store(req, res){
+        const box = await Box.create({title: "Rocket"});
+        return res.json(box);
+    }
+
+    async show(req, res){
+        const box = await Box.findById(req.params.id).populate({
+            path:"files",
+            options:{sort:{createdAt: -1}}
+        });
+
+    }
+}
+
+module.exports = new BoxController();
